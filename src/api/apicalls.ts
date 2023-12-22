d705a928133fa596ba6e98abcc4f6633e7490b95
wibawaarif <arifwb.work@gmail.com>
2023-12-22 14:48:28 +0700

    add: tab navigator

--- src/api/apicalls.ts
+++ src/api/apicalls.ts
@@ -8,7 +8,7 @@ export const nowPlayingMovies: string = `https://api.themoviedb.org/3/movie/now_
 export const upcomingMovies: string = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}`;
 export const popularMovies: string = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
 export const searchMovies = (keyword: string): string => {
-  return `https://api.themoviedb.org/3/search/movie?api_key${apiKey}&query=${keyword}`;
+  return `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${keyword}`;
 };
 export const movieDetails = (id: number): string => {
   return `https://api.themoviedb.org/3/movie/${id}?api_key${apiKey}`;
---
+++ src/navigators/TabNavigator.tsx
@@ -0,0 +1,125 @@
+import React from 'react';
+import Home from '../screens/Home';
+import Search from '../screens/Search';
+import Ticket from '../screens/Ticket';
+import UserAccount from '../screens/UserAccount';
+import {COLORS, FONTSIZE, SPACING} from '../theme/theme';
+import CustomIcon from '../components/CustomIcon';
+import {View, StyleSheet} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarHideOnKeyboard: true,
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: COLORS.Black,
+          borderTopWidth: 0,
+          height: SPACING.space_10 * 10,
+        },
+      }}>
+      <Tab.Screen
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({focused, color, size}) => {
+            return (
+              <View
+                style={[
+                  styles.activeTabBackground,
+                  focused ? {backgroundColor: COLORS.Orange} : {},
+                ]}>
+                <CustomIcon
+                  name="video"
+                  color={COLORS.White}
+                  size={FONTSIZE.size_30}
+                />
+              </View>
+            );
+          },
+        }}
+        name="Home"
+        component={Home}
+      />
+      <Tab.Screen
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({focused, color, size}) => {
+            return (
+              <View
+                style={[
+                  styles.activeTabBackground,
+                  focused ? {backgroundColor: COLORS.Orange} : {},
+                ]}>
+                <CustomIcon
+                  name="search"
+                  color={COLORS.White}
+                  size={FONTSIZE.size_30}
+                />
+              </View>
+            );
+          },
+        }}
+        name="Search"
+        component={Search}
+      />
+      <Tab.Screen
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({focused, color, size}) => {
+            return (
+              <View
+                style={[
+                  styles.activeTabBackground,
+                  focused ? {backgroundColor: COLORS.Orange} : {},
+                ]}>
+                <CustomIcon
+                  name="ticket"
+                  color={COLORS.White}
+                  size={FONTSIZE.size_30}
+                />
+              </View>
+            );
+          },
+        }}
+        name="Ticket"
+        component={Ticket}
+      />
+      <Tab.Screen
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({focused, color, size}) => {
+            return (
+              <View
+                style={[
+                  styles.activeTabBackground,
+                  focused ? {backgroundColor: COLORS.Orange} : {},
+                ]}>
+                <CustomIcon
+                  name="user"
+                  color={COLORS.White}
+                  size={FONTSIZE.size_30}
+                />
+              </View>
+            );
+          },
+        }}
+        name="User"
+        component={UserAccount}
+      />
+    </Tab.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({
+  activeTabBackground: {
+    backgroundColor: COLORS.Black,
+    padding: SPACING.space_18,
+    borderRadius: SPACING.space_18 * 10,
+  },
+});
+
+export default TabNavigator;
